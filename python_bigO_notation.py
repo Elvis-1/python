4237a3eeedc167f02fb66d3c95a9cd6804770e16
Elvis <66243414+Elvis-1@users.noreply.github.com>
2023-11-15 17:02:51 +0100

    added algo and big 0

--- python_bigO_notation.py
+++ python_bigO_notation.py
@@ -22,5 +22,42 @@ def linearSearch(arr,target):
 
 # As the size of the list (arr) increases, the number of iterations the loop performs also increases linearly. Therefore, this algorithm has a time complexity of O(n).
 
+# O(n^2) - Quadratic Time
+
+# Algorithms with quadratic time complexity have runtimes that grow with the square of the input size. As the input data size increases, the runtime increases quadratically.
+
+# Example: Bubble Sort, a simple sorting algorithm.
+
+def bubbleSort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0,n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j + 1] = arr[j], arr[j+1]
+
+#Bubble sort has a time complexity of O(n^2). As the size of the input list (arr) increases, the number of comparisons and swaps grows quadratically.
+
+
+# O(log n) - Logarithmic Time
+
+# Algorithms with logarithmic time complexity have runtimes that grow logarithmically with the size of the input data. Logarithmic time complexity is considered very efficient.
+
+# Example: Binary search in a sorted list.
+
+def binarySearch(arr, target):
+    left, right =0, len(arr) - 1
+    while left <= right:
+        mid = left + right // 2
+        if arr[mid] == target:
+            return target
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+         
+
+
+
 
 
